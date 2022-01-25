bdea5d463e5b48ba623e331618cce0c9c65428d8
Nicolas Gonzales <nico22utn@gmail.com>
2022-01-25 19:34:52 -0300

    feat(init): Initializing project
    - React Navigation was included
    - A simple Home screen was added
    - The project's based on Expo CLI

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet } from 'react-native';
+import Home from './src/screens/Home';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
